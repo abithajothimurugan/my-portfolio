1550da463f5822dd97cdd24878ffc11a9993eafc
Abitha <abithajothimurugan06.com>
2026-02-04 05:38:23 +0530

    Add footer section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
       <Education />
       <Projects />
       <Contact />
+
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="footer-copy">
+                © {new Date().getFullYear()} Abitha Jothi Murugan. All rights reserved.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
